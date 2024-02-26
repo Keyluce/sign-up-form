96e53e09a824ceceafa914621607f9bf4493e6e6
Keyluce <86408122+Keyluce@users.noreply.github.com>
2024-02-27 00:06:02 +0530

    Make changes to the structure of right panel. (Remove flex)

--- javascript.js
+++ javascript.js
@@ -1,28 +1,18 @@
-
 const passwordInputs = document.querySelectorAll('input[type="password"]');
-
 passwordInputs.forEach(input => input.addEventListener('input', matchPasswords));
-
-
-
 
 function matchPasswords()
 {
-
- 
     if (passwordInputs[0].value == passwordInputs[1].value && passwordInputs[0].value != '')
     {
         passwordInputs[0].classList.remove('error');
         passwordInputs[1].classList.remove('error');
     }
-
     else{
         passwordInputs[0].classList.add('error');
         passwordInputs[1].classList.add('error');
     }
 }
-
-
 
 
 
